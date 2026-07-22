466aa7e653899fbd390e8bcc5303a624f74c5864
lucian james <ljames@rivelinrobotics.com>
2026-07-22 13:08:31 +0100

    remove silly title

--- rviz_common/src/rviz_common/visualization_frame.cpp
+++ rviz_common/src/rviz_common/visualization_frame.cpp
@@ -750,7 +750,7 @@ void VisualizationFrame::setDisplayTitleFormat(const QString & title_format)
 void VisualizationFrame::setDisplayConfigFile(const std::string & path)
 {
   display_config_file_ = path;
-  std::string title = "Rivelin Netshape | Hargible Edition";
+  std::string title = "Rivelin Netshape";
 
   setWindowTitle(QString::fromStdString(title));
 }
